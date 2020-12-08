62514d770b8eec783a523293b9771fa29c949da3
andreazure1989@hotmail.fr <extremety1989>
2020-12-08 10:17:05 +0100

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "./src/nectar.hpp"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+using namespace NectarCore;
+using namespace NectarCore::Global;
+using namespace NectarCore::Class;
+using namespace NectarCore::Functions;
+
+
+int main(int argc, char** argv)
+{
+	printl("k");
+	return 0;
+}
